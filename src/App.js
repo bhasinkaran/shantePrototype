4fa3af7f65f6a476bc812593e6805e6cce8d23d1
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-06-27 01:40:11 +0530

    Functional homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import React, {useState, useContext, useEffect} from 'react';
+import {Router } from  'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import HomepageLayout from './components/HomePageLayout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomepageLayout />
     </div>
   );
 }
+// <header className="App-header">
+{/* <img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header> */}
 
 export default App;
