67de36da95af5e1b39e1dfb31239c8f50d020356
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-06-28 14:28:10 +0530

    Setup with firebase working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 import React, {useState, useContext, useEffect} from 'react';
-import {Router } from  'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-import HomepageLayout from './components/HomePageLayout';
+import HomepageLayout from './components/LandingPage';
+import PageHeader from './components/PageHeader'
+import {dbMessages, dbCollegeCounselors, dbColleges, dbStudents, dbHSCounselors} from './firebase/firebase';
+
+
+export const InfoContext = React.createContext();
 
 function App() {
+  const [students, setStudents]=useState("")
+  const [hscounselors, setHSCounselors]=useState("")
+  const [collegecounselors, setCollegeCounselors]=useState("")
+  const [colleges, setColleges]=useState("")
+  const [messages, setMessages]=useState("")
+  const [user, setUser]=useState("");
+
+  
+  function withMenu(page){
+  return (<div>
+          <PageHeader />
+          {page}
+          </div>)}
   return (
-    <div className="App">
-      <HomepageLayout />
-    </div>
+    <BrowserRouter>
+
+      {withMenu(<HomepageLayout />)}
+    </BrowserRouter>
   );
 }
-// <header className="App-header">
-{/* <img src={logo} className="App-logo" alt="logo" />
-<p>
-  Edit <code>src/App.js</code> and save to reload.
-</p>
-<a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Learn React
-</a>
-</header> */}
+
 
 export default App;
---
+++ src/components/PageHeader.js
@@ -0,0 +1,24 @@
+import {Header, Segment, } from 'semantic-ui-react';
+import React from 'react';
+import { Link} from 'react-router-dom';
+
+const PageHeader = () => {
+    return (
+      <Segment
+      basic
+      style={{ backgroundColor: "#1DB954", textAlign: "center", marginBottom: "10px" }}
+      fluid="true">
+          <Header
+            as={Link}
+            to={`/`}
+            inverted 
+            content="PUT COMPANY NAME HERE" 
+            size="large" 
+            color="black"
+            style={{cursor: "default"}}
+          />
+    </Segment>
+    );
+  };
+
+  export default PageHeader;
