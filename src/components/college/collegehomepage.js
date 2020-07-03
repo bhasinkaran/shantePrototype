ea6ee858788c84f4a9dc1458be93055771cb85e2
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-07-03 16:01:13 +0530

    Custom image header on college homepage

--- src/components/college/collegehomepage.js
+++ src/components/college/collegehomepage.js
@@ -7,35 +7,91 @@ import { InfoContext } from '../../App'
 
 const ReturnCollege = () => {
         const { user, students, hscounselors, collegecounselors, colleges, messages, coaches, chats } = React.useContext(InfoContext);
-        const arrayColors = ['red','orange','yellow','olive','green','teal','blue','violet','purple','pink','brown','grey','black'];
-        const number = Math.floor((Math.random() * 12) );
-        const {name}= useParams();
+        const arrayColors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black'];
+        const number = Math.floor((Math.random() * 12));
+        const { name } = useParams();
+        if(colleges)
         return (
-
-                <Grid.Row>
-                        <Grid.Column width={16}>
-                             
-                                <Segment animated color={arrayColors[number]} raised>
-                                <Header color='teal' style={{ marginTop: "-5px" }} >{colleges[name].name}</Header>
-                                <Header color='violet' style={{ marginTop: "-10px" }} >{colleges[name].targetlocations}</Header>
-                                <Grid.Row>
-                                        <Grid>
-                                                <Grid.Column width={8}>
-                                                        <Header style={{ marginTop: "-5px" }} as="h4">Popular Areas of Study</Header>
-                                                        {colleges[name].targetmajors.slice(0,3).map(major => <Header as={'h5'} style={{ marginTop: "-10px" }}>{major}</Header>)}
-                                                </Grid.Column>
-                                                <Grid.Column width={8}>
-                                                        <Header textAlign={"center"} style={{ marginTop: "-5px" }} as="h4">Tuition</Header>
-                                                        <Header textAlign={"center"} style={{ marginTop: "-10px" }} as="h5">{"$" + colleges[name].tuition} {colleges[name].aid ? " offering " : " with no "}  {"need-based aid"}</Header>
-
-                                                </Grid.Column>
-                                        </Grid>
-                                </Grid.Row>
-                                </Segment>
+             
+                <Grid>
+                        <Grid.Column width={4}>
+                                <Image size="huge" src={colleges[name].logopic}></Image>
                         </Grid.Column>
-                </Grid.Row>
+                        <Grid.Column width={12}>
+                                <Grid.Row>
+                                        
+                                        
+                                                 <img  src={colleges[name].collegepic}/>
+                                                
 
+                                        {/* <Image src={colleges[name].collegepic}></Image> */}
+                                </Grid.Row>
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row>
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row>
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row>
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row> 
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row> 
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row> 
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row>
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row>
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row>
+                                <Grid.Row>
+                                       Test
+                                </Grid.Row>
+                                <Grid.Row>
+                                       
+                                </Grid.Row>
+                                {/* <Divider>
 
+                                </Divider>
+                                <Divider>
+                                        
+                                </Divider>
+                                <Divider>
+                                        
+                                </Divider> */}
+                                <Grid.Row>
+                                        <Grid.Column width={16}>
+                                                <Segment animated color={arrayColors[number]} raised>
+                                                        <Header color='teal' style={{ marginTop: "-5px" }} >{colleges[name].name}</Header>
+                                                        <Header color='violet' style={{ marginTop: "-10px" }} >{colleges[name].targetlocations}</Header>
+                                                        <Grid.Row>
+                                                                <Grid.Column width={8}>
+                                                                        <Header style={{ marginTop: "-5px" }} as="h4">Popular Areas of Study</Header>
+                                                                        {colleges[name].targetmajors.slice(0, 3).map(major => <Header as={'h5'} style={{ marginTop: "-10px" }}>{major}</Header>)}
+                                                                </Grid.Column>
+                                                                <Grid.Column width={8}>
+                                                                        <Header textAlign={"center"} style={{ marginTop: "-5px" }} as="h4">Tuition</Header>
+                                                                        <Header textAlign={"center"} style={{ marginTop: "-10px" }} as="h5">{"$" + colleges[name].tuition} {colleges[name].aid ? " offering " : " with no "}  {"need-based aid"}</Header>
+
+                                                                </Grid.Column>
+                                                        </Grid.Row>
+                                                </Segment>
+                                        </Grid.Column>
+                                </Grid.Row>
+                        </Grid.Column>
+                </Grid>
         );
+        else{
+                return "Loading"
+        }
 }
 export default ReturnCollege;
