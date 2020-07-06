8aa9b4ef74c172cd7c0ce0c27c2690094edc6d78
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-07-06 22:10:46 +0530

    Good to go

--- src/components/student/homepage/Modals/CreateRequest.js
+++ src/components/student/homepage/Modals/CreateRequest.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Container, Form, Header, Divider, Grid, Loader, Button, Image, Modal, Icon, Input } from 'semantic-ui-react'
 import { Router, useParams } from 'react-router-dom';
 import { InfoContext } from '../../../../App'
-import { dbStudents, dbRequests } from '../../../../firebase/firebase'
+import { dbStudents, dbRequests, dbCoaches } from '../../../../firebase/firebase'
 import SemanticDatepicker from 'react-semantic-ui-datepickers';
 import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
 
@@ -13,18 +13,18 @@ const ModalRequest = ({ open, setOpen }) => {
         const [subject, setSubject] = useState("");
         const [coach, setCoach] = useState("");
         const [priority, setPriority] = useState("");
-        const[coachesList,setCoachesList]=useState([]);
+        const [body, setBody] = useState("");
+        const [coachesList,setCoachesList]=useState([]);
         const [date, setDate] = useState(new Date());
         useEffect(()=>{
-                if(students&& students[user] &&students[user]['coaches']){
+                if(students&& students[user] &&students[user]['coaches'] && coaches){
                         var temp=[];
                         var index=0;
-                        Object.values(students[user]['coaches']).map(name=>temp.push({key: temp.length, value:name, text:name }));
+                        Object.values(students[user]['coaches']).map(name=>temp.push({key: temp.length, value:name, text:coaches[name]['firstName'] }));
                         console.log(temp);
                         setCoachesList(temp);
                 }
-                
-        }, [user]);
+        }, [user, coaches]);
         const priorities = [
                 {
                         key: 0,
@@ -53,8 +53,13 @@ const ModalRequest = ({ open, setOpen }) => {
                         day: date.getDay(),
                         year: date.getFullYear(),
                         month: date.getMonth(), // returns month based on 0
-                        time: date.getTime()
+                        time: date.getTime(),
+                        body: body
                 })
+                const  specialKey = key.getKey();
+                dbStudents.child(user).child('requests').push(specialKey);
+                dbCoaches.child(coach).child('requests').push(specialKey);
+
         }
         const TestDate = () => {
                 const onChange = (e, data) => {
@@ -77,40 +82,63 @@ const ModalRequest = ({ open, setOpen }) => {
                         <Header icon='archive' content='Create Request' />
                         <Modal.Content>
                                 <Form>
-                                        <Form.Group widths='equal'>
+                                        <Form.Group inline widths='equal'>
                                                 
                                                 <Form.Select 
                                                 fluid
                                                 label='To'
                                                 options={coachesList}
                                                 required={true}
-                                                placeholder='Gender'
-                                                // onChange={(e, { value }) => {
-                                                //   setGender(value)
-                                                //   console.log(value);
-                                                // }}
+                                                placeholder='Choose Coach'
+                                                onChange={(e, { value }) => {
+                                                  setCoach(value)
+                                                  console.log(value);
+                                                }}
+
                                                 // fluid label='To' required placeholder='Enter Coach.' 
                                                 
                                                 
                                                 />
-                                                <Form.Input fluid label='Last name' placeholder='Last name' />
-                                                {/* <Form.Select
-                                                        fluid
-                                                        label='Gender'
-                                                        options={options}
-                                                        placeholder='Gender'
-                                                /> */}
+
+<Form.Select 
+                                                fluid
+                                                label='Priority'
+                                                options={priorities}
+                                                required={true}
+                                                placeholder='Choose Priority'
+                                                onChange={(e, { value }) => {
+                                                  setPriority(value)
+
+                                                }}
+                                                // fluid label='To' required placeholder='Enter Coach.' 
+                                                
+                                                
+                                                />
+                                                
+                                                
                                         </Form.Group>
+                                        <Header as='h5' style={{marginBottom: "2px"}} >Due Date</Header>
+                                               <TestDate />
                                 </Form>
-                                <Header color="teal">
-                                        Your coaches will be notified when you register and meet this deadline!
-                                </Header>
-                                <Input value={subject} onChange={(e, { value }) => setSubject(value)} placeholder='Enter Subject...' />
                                 <Divider></Divider>
-                                <Header color="brown">
-                                        Enter Deadline Due-Date:
-                                </Header>
-                                <TestDate />
+                                <Form>
+                                        
+                                                <Form.Input label="Subject" required={true} value={subject} onChange={(e, { value }) => setSubject(value)} placeholder='Enter Subject...'>
+                                                </Form.Input>
+                                               
+                                                
+                                        
+                                </Form>
+                                <br />
+                                <Form>
+                                        
+                                        <Form.TextArea rows={3} label="Body" required={true}  value={body} onChange={(e, { value }) => setBody(value)} placeholder='Enter Body...'>
+                                                </Form.TextArea>
+
+                                               
+                                               
+                                        
+                                </Form>
 
                         </Modal.Content>
                         <Modal.Actions>
